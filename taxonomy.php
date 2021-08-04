c2b5a1e26d7a368a1767e5d540ad8ea01a16d98e
Johan van der Wijk <johan@vanderwijk.nl>
2021-08-04 11:56:40 +0200

    Temporary

--- taxonomy.php
+++ taxonomy.php
@@ -28,7 +28,7 @@ function country() {
 		'labels'                     => $labels,
 		'hierarchical'               => true,
 		'public'                     => true,
-		'show_ui'                    => false,
+		'show_ui'                    => true,
 		'show_admin_column'          => true,
 		'show_in_nav_menus'          => true,
 		'show_tagcloud'              => true,
