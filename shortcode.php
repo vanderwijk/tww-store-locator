cb957013f5231a886d6ca0d0260ddcff354a5c12
Johan van der Wijk <vanderwijk@users.noreply.github.com>
2021-08-10 14:53:39 +0200

    Map size

--- shortcode.php
+++ shortcode.php
@@ -96,7 +96,7 @@ function showstorelocator_shortcode() {
 		var mapOptions = {
 			mapId: 'c62305ec4f432eb',
 			zoom: 3,
-			center: new google.maps.LatLng(40, -15),
+			center: new google.maps.LatLng(45, -15),
 			mapTypeId: 'roadmap'
 		};
 		map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
